391cec2313f1a562cb6319be4d88efcae0e420da
김재은 <wodmsdl0117@gmail.com>
2025-05-16 12:07:28 +0900

    250516_오전 수업

--- workspace_sql/250512_SELECT.sql
+++ workspace_sql/250512_SELECT.sql
@@ -41,25 +41,38 @@ select * from emp order by deptno asc, sal desc;
 select * from emp order by deptno asc, sal desc, empno desc;
 
 /* 250513 - 오후 시간대 진행(조퇴 부분) */
-select * from emp where deptno = 30; -- WHERE절
+select * from emp 
+    where deptno = 30; -- WHERE절
 
-select * from emp where deptno = 30 order by sal; -- order by 절보다 앞에
+select * from emp 
+    where deptno = 30 order by sal; -- order by 절보다 앞에
 
-select * from emp where sal = 1600;
-select * from emp where sal >= 1600;
-select * from emp where  sal <=1600; -- 초과, 미만일 때는 = 제거
+select * from emp 
+    where sal = 1600;
+select * from emp 
+    where sal >= 1600;
+select * from emp 
+    where  sal <=1600; -- 초과, 미만일 때는 = 제거
 
-select * from emp where deptno != 30; -- !=는 <>와 같은 의미
+select * from emp 
+    where deptno != 30; -- !=는 <>와 같은 의미
 
-select * from emp where deptno = 30 and job = 'SALESMAN'; -- 둘 다 만족
-select * from emp where deptno = 30 or job = 'CLERK';
+select * from emp 
+    where deptno = 30 and job = 'SALESMAN'; -- 둘 다 만족
+select * from emp 
+    where deptno = 30 or job = 'CLERK';
 
-select * from emp where job='CLERK' or (sal>2000 and deptno = 10);
-select * from emp where sal >= 2000 and sal <4000; -- 일반 수학 비교처럼 'n<칼럼명<m'으로 쓰면 오류가 뜨는 듯
-select * from emp where deptno = 10 or deptno = 20;
+select * from emp 
+    where job='CLERK' or (sal>2000 and deptno = 10);
+select * from emp 
+    where sal >= 2000 and sal <4000; -- 일반 수학 비교처럼 'n<칼럼명<m'으로 쓰면 오류가 뜨는 듯
+select * from emp 
+    where deptno = 10 or deptno = 20;
 
-select * from emp where deptno in (10, 20);
-select * from emp where deptno not in (10, 20); --앞에 not을 붙여 부정으로
+select * from emp 
+    where deptno in (10, 20);
+select * from emp 
+    where deptno not in (10, 20); --앞에 not을 붙여 부정으로
 
 
 
@@ -67,75 +80,125 @@ select * from emp where deptno not in (10, 20); --앞에 not을 붙여 부정으
 select * from emp; -- 테이블 확인용
 
 -- 1. 부서번호 10번인 사람들을 출력 (O)
-select * from emp where deptno = 10;
+select * from emp 
+    where deptno = 10;
 
 -- 2. 부서번호 10번이 아닌 사람들을 출력 (O)
-select * from emp where not (deptno = 10); 
-select * from emp where deptno != 10;
+select * from emp 
+    where not (deptno = 10); 
+select * from emp 
+    where deptno != 10;
 
 -- 3. 급여가 3000 이상인 사람들을 출력 (O)
-select * from emp where sal>=3000; 
+select * from emp 
+    where sal>=3000; 
 
 -- 4. 급여가 1500~3000 사이(포함)의 사람을 출력 (O)
-select * from emp where sal>=1500 and sal<=3000; 
-select * from emp where sal between 1500 and 3000; -- 사용 제한이 있는 언어가 있음
+select * from emp 
+    where sal>=1500 and sal<=3000; 
+select * from emp 
+    where sal between 1500 and 3000; -- 사용 제한이 있는 언어가 있음
 
 -- 5. 부서번호 10번인 사람 중 급여 2000 이상인 사람을 출력 (O)
-select * from emp where deptno = 10 and sal >=2000; 
+select * from emp 
+    where deptno = 10 and sal >=2000; 
 
 -- 6. 부서번호 30번 중 1500~3000 사이(미포함)인 사람을 출력 (O)
-select * from emp where deptno = 30 and (sal > 1500 and sal < 3000); 
+select * from emp 
+    where deptno = 30 and (sal > 1500 and sal < 3000); 
 
 -- 7. 부서번호 30번 중 1500~3000 사이(미포함)인 사람을 연봉이 작은 순으로 출력, 연봉이 같은 경우 이름이 빠른 순으로 출력 (O)
-select * from emp where deptno = 30 and (sal > 1500 and sal < 3000) order by sal asc, ename asc; 
+select * from emp 
+    where deptno = 30 and (sal > 1500 and sal < 3000) 
+    order by sal asc, ename asc; 
 
 -- 8. 부서번호 20,30번 중 1500~3000 사이(미포함)인 사람을 연봉이 작은 순으로 출력, 연봉이 같은 경우 이름이 빠른 순으로 출력 (O)
-select * from emp where deptno in (20, 30) and (sal > 1500 and sal < 3000) order by sal asc, ename asc; 
-select * from emp where (deptno = 20 or deptno = 30) and (sal > 1500 and sal < 3000) order by sal asc, ename asc; -- or 사용시
+select * from emp 
+    where deptno in (20, 30) and (sal > 1500 and sal < 3000) 
+    order by sal asc, ename asc; 
+select * from emp 
+    where (deptno = 20 or deptno = 30) and (sal > 1500 and sal < 3000) 
+    order by sal asc, ename asc; -- or 사용시
 
 -- 9. 부서번호가 20 또는 30이고 급여가 1500 이상인 직원의 이름과 급여를 급여 오름차순으로 출력 (X) - 문제 잘 확인하기!!!! 전 칼럼이 아닌 특정 칼럼만 출력!
-select ename, sal from emp where deptno in (20, 30) and sal >= 1500 order by sal asc; 
+select ename, sal from emp 
+    where deptno in (20, 30) and sal >= 1500 
+    order by sal asc; 
 
 
 /* 250514 수업 */
-select * from emp where sal between 2000 and 3000;
-select * from emp where (sal between 2000 and 3000) and deptno = 20;
-select * from emp where sal not between 2000 and 3000;
+select * from emp 
+    where sal between 2000 and 3000;
+select * from emp 
+    where (sal between 2000 and 3000) and deptno = 20;
+select * from emp 
+    where sal not between 2000 and 3000;
 
-select * from emp where (sal between 2000 and 3000) and (sal != 2000 and sal != 3000);
-select * from emp where (sal between 2000+1 and 3000-1); -- 이쪽은 소수점 값일 때 맞지 않는 경우 있으니 가급적 쓰지 않기
+select * from emp 
+    where (sal between 2000 and 3000) and (sal != 2000 and sal != 3000);
+select * from emp 
+    where (sal between 2000+1 and 3000-1); -- 이쪽은 소수점 값일 때 맞지 않는 경우 있으니 가급적 쓰지 않기
 
-select * from emp where ename like 'S%';
-select * from emp where ename like '_L%';
+select * from emp 
+    where ename like 'S%';
+select * from emp 
+    where ename like '_L%';
 
-select * from emp where ename like '%AM%';
-select * from emp where ename like '%E%';
-select * from emp where ename like '_%E%_';
-select * from emp where ename like '%A%';
-select * from emp where ename like '_%A%_';
+select * from emp 
+    where ename like '%AM%';
+select * from emp 
+    where ename like '%E%';
+select * from emp 
+    where ename like '_%E%_';
+select * from emp 
+    where ename like '%A%';
+select * from emp 
+    where ename like '_%A%_';
 
-select * from emp where ename not like '%A%';
+select * from emp 
+    where ename not like '%A%';
 
-select * from emp where ename like '%A%' and ename like '%S%'; -- 나중에 확인
-select * from emp where ename like '%A%S%' or ename like '%S%A%';
+select * from emp 
+    where ename like '%A%' and ename like '%S%'; -- 나중에 확인
+select * from emp 
+    where ename like '%A%S%' or ename like '%S%A%';
 
-select * from emp where ename like '%A%' or ename like '%S%';
+select * from emp 
+    where ename like '%A%' or ename like '%S%';
 
 select comm from emp;
 
-select comm from emp where comm >= 400;
+select comm from emp 
+    where comm >= 400;
 
-select comm from emp where comm = null;
-select comm from emp where comm is not null;
-select comm from emp where comm is null;
+select comm from emp 
+    where comm = null;
+select comm from emp 
+    where comm is not null;
+select comm from emp 
+    where comm is null;
 
-select * from emp where deptno = 10;
-select * from emp where deptno = 20;
+select * from emp 
+    where deptno = 10;
+select * from emp 
+    where deptno = 20;
 
-select * from emp where deptno = 10 union select * from emp where deptno = 20;
-select * from emp where deptno = 10 union select * from emp where deptno = 10;
+select * from emp 
+    where deptno = 10 
+union 
+select * from emp 
+    where deptno = 20;
+select * from emp 
+    where deptno = 10 
+union 
+select * from emp 
+    where deptno = 10;
 
-select * from emp where deptno = 10 union all select * from emp where deptno = 10;
+select * from emp 
+    where deptno = 10 
+union all 
+select * from emp 
+    where deptno = 10;
 
 select empno from emp
 union all
@@ -185,8 +248,10 @@ select * from emp
 
 select ename, upper(ename), lower(ename), initcap(ename) from emp;
 
-select ename from emp where ename like '%aM%'; -- 이 상태로는 출력 X
-select ename from emp where lower(ename) like lower('%aM%'); -- 이렇게 where절 조건을 맞춰주면 출력 가능
+select ename from emp 
+    where ename like '%aM%'; -- 이 상태로는 출력 X
+select ename from emp 
+    where lower(ename) like lower('%aM%'); -- 이렇게 where절 조건을 맞춰주면 출력 가능
 
 select * from dual; -- 연습용 더미 테이블
 select 1 from dual;
@@ -197,42 +262,69 @@ select upper('aBc'), upper(lower('aBc')) from dual;
 
 select ename from emp;
 select ename, length(ename) from emp;
-select * from emp where length(ename) = 5; -- 이름 글자수가 5자인 경우만 출력
+select * from emp 
+    where length(ename) = 5; -- 이름 글자수가 5자인 경우만 출력
 
 select lengthb('a'), lengthb('한') from dual;
 
 desc emp;
 
 /* 250514 - 자체 복습 */
-SELECT * FROM EMP WHERE DEPTNO BETWEEN 20 AND 30; -- between 연산자
-SELECT * FROM EMP WHERE JOB LIKE '%T%'; --LIKE 연산자
-SELECT * FROM EMP WHERE (JOB LIKE '%T%') AND (DEPTNO BETWEEN 20 AND 30); -- AND로 위 둘을 합쳐보기... 세미콜론 잊지 말기!
-SELECT * FROM EMP WHERE COMM IS NULL;
+SELECT * FROM EMP 
+    WHERE DEPTNO BETWEEN 20 AND 30; -- between 연산자
+SELECT * FROM EMP 
+    WHERE JOB LIKE '%T%'; --LIKE 연산자
+SELECT * FROM EMP 
+    WHERE (JOB LIKE '%T%') AND (DEPTNO BETWEEN 20 AND 30); -- AND로 위 둘을 합쳐보기... 세미콜론 잊지 말기!
+SELECT * FROM EMP 
+    WHERE COMM IS NULL;
 
 SELECT LOWER(JOB) FROM EMP;
-SELECT JOB FROM EMP WHERE LENGTH(JOB) = 5;
-SELECT LOWER(JOB) FROM EMP WHERE LENGTH(JOB) = 5;
+SELECT JOB FROM EMP 
+    WHERE LENGTH(JOB) = 5;
+SELECT LOWER(JOB) FROM EMP 
+    WHERE LENGTH(JOB) = 5;
 
 /* 250514 복습용 예제 다시 풀이 */
-SELECT * FROM EMP WHERE ENAME LIKE '%S';
-SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE DEPTNO = 30 AND JOB = 'SALESMAN';
-SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE DEPTNO IN (20, 30) AND SAL > 2000;
-SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE DEPTNO = 20 AND SAL > 2000
+SELECT * FROM EMP 
+    WHERE ENAME LIKE '%S';
+    
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP 
+    WHERE DEPTNO = 30 AND JOB = 'SALESMAN';
+    
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP 
+    WHERE DEPTNO IN (20, 30) AND SAL > 2000;
+    
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP 
+    WHERE DEPTNO = 20 AND SAL > 2000
 UNION
-SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP WHERE DEPTNO = 30 AND SAL > 2000;
-SELECT * FROM EMP WHERE SAL<2000 OR SAL>3000;
-SELECT ENAME, EMPNO, SAL, DEPTNO FROM EMP WHERE ENAME LIKE '%E%' AND DEPTNO = 30 AND SAL NOT BETWEEN 1000 AND 2000;
-SELECT * FROM EMP WHERE COMM IS NULL AND MGR IS NOT NULL AND JOB IN ('MANAGER', 'CLERK') AND ENAME NOT LIKE '_L%';
+SELECT EMPNO, ENAME, JOB, SAL, DEPTNO FROM EMP 
+    WHERE DEPTNO = 30 AND SAL > 2000;
+    
+SELECT * FROM EMP 
+    WHERE SAL<2000 OR SAL>3000;
+SELECT ENAME, EMPNO, SAL, DEPTNO FROM EMP 
+    WHERE ENAME LIKE '%E%' AND DEPTNO = 30 AND SAL NOT BETWEEN 1000 AND 2000;
+SELECT * FROM EMP 
+    WHERE COMM IS NULL AND MGR IS NOT NULL AND JOB IN ('MANAGER', 'CLERK') AND ENAME NOT LIKE '_L%';
 
 
 /* 250515 수업 */
 
-select * from emp where deptno = 10 order by empno desc;
-select * from emp where deptno = 20 order by empno asc; -- 이 문장들을 union이나 union all로,
+select * from emp 
+    where deptno = 10 
+    order by empno desc;
+select * from emp 
+    where deptno = 20 
+    order by empno asc; -- 이 문장들을 union이나 union all로,
 
-select * from emp where deptno = 10 order by empno desc
+select * from emp 
+    where deptno = 10 
+    order by empno desc
 union all
-select * from emp where deptno = 20 order by empno asc; -- 합치기 불가
+select * from emp 
+    where deptno = 20 
+    order by empno asc; -- 합치기 불가
 
 select job from emp; -- 전체 열 확인용
 select ename from emp; -- 문제 답 확인용
@@ -296,18 +388,89 @@ select to_char(hiredate, 'yyyy"년" mm"월" dd"일"') as HIREDATE from emp;
 
 select to_date('2025-05-15', 'yyyy-mm-dd') - to_date('2025-05-12', 'yyyy-mm-dd') from dual;
 
-select * from emp where hiredate > to_date('1981-06-01', 'yyyy-mm-dd') order by hiredate;
+select * from emp 
+    where hiredate > to_date('1981-06-01', 'yyyy-mm-dd') order by hiredate;
 
 select sal*12 + nvl(comm, 0) as pay from emp;
 
-select empno, rpad(substr(empno, 1, 2), length(empno), '*') as MASKING_EMPNO, ename, rpad(substr(ename, 1, 1), length(ename), '*') as MASKING_ENAME from emp where length(ename) >=5 and length(ename)<6;
-select empno, ename, sal, trunc(sal/172*8, 2) as DAY_PAY, round(sal/172, 1) AS TIME_PAY from emp;
-select empno, ename, hiredate, to_char(add_months(hiredate, 3), 'yyyy-mm-dd') as R_JOB, nvl(to_char(comm), 'N/A') as COMM from emp;
+select * from emp E, salgrade S 
+    where E.sal between S.losal and S.hisal;
+
+
+/* 250516 수업 */
+
+select ename, sal*12+nvl(comm, 0) as total_pay from emp; -- Q. 각 사원의 연봉 출력. 월급*12+comm. ename, total_pay로 출력
+
+select empno, ename, job, sal,
+    decode(job,
+        'MANAGER', SAL*1.1,
+        'SALESMAN', SAL*1.05,
+        'ANALYST', SAL,
+        SAL*1.03) as upsal -- 표기 없는 이 부분이 나머지 경우
+from emp;
+
+select job, sal,
+    decode(job,
+        'MANAGER', SAL*1.1,
+        'SALESMAN', SAL*1.05,
+        'ANALYST', SAL,
+        SAL*1.03) as upsal
+from emp;
+
+select job, sal,
+    case job
+        when 'MANAGER' then SAL*1.1
+        when 'SALESMAN' then SAL*1.05
+        when 'ANALYST' then SAL
+        else SAL*1.03
+        end as upsal
+from emp;
+
+
+--nvl 사용 없이 decode, case로 nvl과 동일 기능 구현
+
+select comm,
+    decode(comm, 
+        null, -1,
+        comm) as decode,
+    case
+        when comm is null then -1 -- when null은 where comm = null과 동일한 의미. comm is null도 사용 불가.
+        else comm
+        end as case
+from emp;
+
+select empno, ename, comm,
+    case
+        when comm is null then '해당 없음'
+        when comm = 0 then '0원'
+        when comm > 0 then '수당 : ' || COMM
+    end as comm_text
+from emp;
+
+select empno,
+rpad(substr(empno, 1, 2), 4, '*') as masking_empno,
+ename,
+rpad(substr(ename, 1, 1), length(ename), '*') as masking_ename
+from emp
+where length(ename) >= 5 and length(ename) < 6;-- Q1.
+
+select empno, ename, sal,
+trunc(sal/(21.5*8)*8, 2) as day_pay,
+round(sal/(21.5*8), 1) as time_pay
+from emp; -- Q2.
+
+select empno, ename, hiredate,
+    to_char(add_months(hiredate, 3), 'yyyy-mm-dd') as r_job,
+    nvl(to_char(comm), 'N/A') as comm
+from emp;-- Q3.
+
 select empno, ename, mgr,
     case
-        when mgr is null then '0000'
-        when mgr like '75__' then '5555'
-        when mgr like '76__' then '6666'
-        when mgr like '77__' then '7777'
-        when mgr like '78__' then '8888'
-        
+        when comm is null then '0000'
+        when comm like '75%' then '5555'
+        when comm like '76%' then '6666'
+        when comm like '77%' then '7777'
+        when comm like '78%' then '8888'
+        else to_char(mgr)
+    end as chg_mgr
+from emp;-- Q4.
