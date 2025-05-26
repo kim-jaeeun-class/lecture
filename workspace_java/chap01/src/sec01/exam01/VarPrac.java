843a5f18cb61688c715666c32d351d545a85bb8c
김재은 <wodmsdl0117@gmail.com>
2025-05-26 16:56:06 +0900

    250526_수업 내용

---
+++ workspace_java/chap01/src/sec01/exam01/VarPrac.java
@@ -0,0 +1,17 @@
+package sec01.exam01;
+
+public class VarPrac {
+
+	public static void main(String[] args) {
+		int v1 = 0;
+		if (v1 = true) {
+			int v3 = 0;
+			v1 = 1;
+			v2 = 1;
+			v3 = 1;
+		}
+		v1 = v2 + v3;
+		System.out.println(v1);
+	}
+
+}
