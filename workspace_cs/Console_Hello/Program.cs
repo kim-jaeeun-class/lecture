d4a440743605d704d75b31e3eb8faee851e2aae7
DESKTOP-HDGPUFR\admin <wodmsdl0117@gmail.com>
2025-09-26 16:52:35 +0900

    250926_수업 내용(c# 시작)

---
+++ workspace_cs/Console_Hello/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void main(String[] args)
+    {
+        int[] arr = { 1, 2, 3, 4, 5 };
+
+        foreach(int i in arr)
+        {
+            Console.WriteLine(i + ",");
+        }
+
+        test();
+    }
+
+    void static void test()
+    {
+        Console.WriteLine("test");
+    }
+}
